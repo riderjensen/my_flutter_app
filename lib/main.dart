b4c89dc3fc3ee216fe5551c9ef5d1934276cfe06
Unknown <riderjensen@gmail.com>
2018-12-17 17:27:34 -0700

    Adding debugging lines for visuals

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,20 @@
 import 'package:flutter/material.dart';
+// import 'package:flutter/rendering.dart';
 
 import './product_manager.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  // debugPaintSizeEnabled = true;
+  // debugPaintPointersEnabled = true;
+  // debugPaintBaselinesEnabled = true;
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      // debugShowMaterialGrid: true,
       theme: ThemeData(
           primarySwatch: Colors.grey,
           accentColor: Colors.teal,
