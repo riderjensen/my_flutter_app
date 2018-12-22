8d2b4c8f23549bde2aff257b69032c110cf2297d
Unknown <riderjensen@gmail.com>
2018-12-22 15:27:15 -0700

    Products now more dynamic

--- lib/products.dart
+++ lib/products.dart
@@ -1,34 +1,52 @@
 import 'package:flutter/material.dart';
 
+import './pages/product.dart';
+
 class Products extends StatelessWidget {
-  final List<String> products;
+  final List<Map<String, String>> products;
+  final Function deleteProduct;
 
-  Products([this.products = const []]);
+  Products(this.products, {this.deleteProduct});
 
-	Widget _buildProductItem(BuildContext context, int index) {
-		return Card(
-                child: Column(
-                  children: <Widget>[
-                    Image.asset('assets/food.jpg'),
-                    Text(products[index])
-                  ],
-                ),
-              );
-	}
+  Widget _buildProductItem(BuildContext context, int index) {
+    return Card(
+      child: Column(
+        children: <Widget>[
+          Image.asset(products[index]['image']),
+          Text(products[index]['title']),
+          ButtonBar(
+            alignment: MainAxisAlignment.center,
+            children: <Widget>[
+              FlatButton(
+                child: Text('Details'),
+                onPressed: () => Navigator.pushNamed<bool>(
+                            context, '/product/' + index.toString())
+                        .then((bool value) {
+                      if (value) {
+                        deleteProduct(index);
+                      }
+                    }),
+              )
+            ],
+          )
+        ],
+      ),
+    );
+  }
 
-	Widget _buildProductsList() {
-		Widget productCard = Center(child: Text('No products found.'));
-	  	if (products.length > 0 ) {
-		  	productCard = ListView.builder(
-			itemBuilder: _buildProductItem,
-			itemCount: products.length,
-		);
-	  }
+  Widget _buildProductsList() {
+    Widget productCard = Center(child: Text('No products found.'));
+    if (products.length > 0) {
+      productCard = ListView.builder(
+        itemBuilder: _buildProductItem,
+        itemCount: products.length,
+      );
+    }
     return productCard;
-	}
+  }
 
   @override
   Widget build(BuildContext context) {
-	  return _buildProductsList();
+    return _buildProductsList();
   }
 }
