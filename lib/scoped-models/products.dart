b8ffc9bc535c9c16cec9ba9ae92d5ccbda944266
Unknown <riderjensen@gmail.com>
2018-12-24 15:03:17 -0700

    Adding favorites to scoped model

--- lib/scoped-models/products.dart
+++ lib/scoped-models/products.dart
@@ -5,8 +5,17 @@ import '../models/product.dart';
 class ProductsModel extends Model {
   List<Product> _products = [];
   int _selectedProductIndex;
+  bool _showFavorites = false;
 
   List<Product> get products {
+    return List.from(_products);
+  }
+
+  List<Product> get displayedProducts {
+    if (_showFavorites) {
+      return List.from(
+          _products.where((Product product) => product.isFavorite).toList());
+    }
     return List.from(_products);
   }
 
@@ -21,22 +30,48 @@ class ProductsModel extends Model {
     return _products[_selectedProductIndex];
   }
 
+  bool get displayFavoritesOnly {
+    return _showFavorites;
+  }
+
   void addProduct(Product product) {
     _products.add(product);
     _selectedProductIndex = null;
+    notifyListeners();
   }
 
   void updateProduct(Product product) {
     _products[_selectedProductIndex] = product;
     _selectedProductIndex = null;
+    notifyListeners();
   }
 
   void deleteProduct() {
     _products.removeAt(_selectedProductIndex);
     _selectedProductIndex = null;
+    notifyListeners();
+  }
+
+  void toggleProductFavorite() {
+    final bool isCurrentlyFavorite = selectedProduct.isFavorite;
+    final bool newFavoriteStatus = !isCurrentlyFavorite;
+    final Product updatedProduct = Product(
+        title: selectedProduct.title,
+        description: selectedProduct.description,
+        price: selectedProduct.price,
+        image: selectedProduct.image,
+        isFavorite: newFavoriteStatus);
+    _products[selectedProductIndex] = updatedProduct;
+    notifyListeners();
   }
 
   void selectProduct(int index) {
     _selectedProductIndex = index;
+    notifyListeners();
+  }
+
+  void toggleDisplayFavorites() {
+    _showFavorites = !_showFavorites;
+    notifyListeners();
   }
 }
