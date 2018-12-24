f851b82420fa164a6312cda0c47fad9ea6cfdc85
Unknown <riderjensen@gmail.com>
2018-12-24 13:04:05 -0700

    Refactoring

--- lib/pages/product_list.dart
+++ lib/pages/product_list.dart
@@ -5,29 +5,53 @@ import './product_edit.dart';
 class ProductListPage extends StatelessWidget {
   final List<Map<String, dynamic>> products;
   final Function updateProduct;
+  final Function deleteProduct;
 
-  ProductListPage(this.products, this.updateProduct);
+  ProductListPage(this.products, this.updateProduct, this.deleteProduct);
+
+  Widget _buildEditButton(BuildContext context, int index) {
+    return IconButton(
+      icon: Icon(Icons.edit),
+      onPressed: () {
+        Navigator.of(context).push(
+          MaterialPageRoute(builder: (BuildContext context) {
+            return ProductEditPage(
+                product: products[index],
+                updateProduct: updateProduct,
+                productIndex: index);
+          }),
+        );
+      },
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
       itemBuilder: (BuildContext context, int index) {
-        return ListTile(
-            leading: Image.asset(products[index]['image']),
-            title: Text(products[index]['title']),
-            trailing: IconButton(
-              icon: Icon(Icons.edit),
-              onPressed: () {
-                Navigator.of(context).push(
-                  MaterialPageRoute(builder: (BuildContext context) {
-                    return ProductEditPage(
-                        product: products[index],
-                        updateProduct: updateProduct,
-                        productIndex: index);
-                  }),
-                );
-              },
-            ));
+        return Dismissible(
+          key: Key(products[index]['title']),
+          onDismissed: (DismissDirection direction) {
+            if (direction == DismissDirection.startToEnd) {
+              deleteProduct(index);
+            }
+          },
+          background: Container(
+            color: Colors.red,
+          ),
+          child: Column(
+            children: <Widget>[
+              ListTile(
+                  leading: CircleAvatar(
+                    backgroundImage: AssetImage(products[index]['image']),
+                  ),
+                  title: Text(products[index]['title']),
+                  subtitle: Text('\$${products[index]['price'].toString()}'),
+                  trailing: _buildEditButton(context, index)),
+              Divider()
+            ],
+          ),
+        );
       },
       itemCount: products.length,
     );
