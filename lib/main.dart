e4a8b1fefd6b5dd0501e90a6f6b4947c561fbff6
Unknown <riderjensen@gmail.com>
2018-12-25 09:38:32 -0700

    Replacing selected product in main

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@ import './pages/products_admin.dart';
 import './pages/products.dart';
 import './pages/product.dart';
 import './scoped-models/main.dart';
+import './models/product.dart';
 
 void main() {
   runApp(MyApp());
@@ -42,9 +43,13 @@ class _MyAppState extends State<MyApp> {
             return null;
           }
           if (pathElements[1] == 'product') {
-            final int index = int.parse(pathElements[2]);
+            final String productId = pathElements[2];
+            final Product product =
+                model.allProducts.firstWhere((Product product) {
+              return product.id == productId;
+            });
             return MaterialPageRoute<bool>(
-              builder: (BuildContext context) => ProductPage(index),
+              builder: (BuildContext context) => ProductPage(product),
             );
           }
           return null;
