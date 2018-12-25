3a5f64b90c7924bb539005d25010fb90135a60db
Unknown <riderjensen@gmail.com>
2018-12-25 13:40:24 -0700

    Changing route to load log out page

--- lib/main.dart
+++ lib/main.dart
@@ -39,11 +39,8 @@ class _MyAppState extends State<MyApp> {
             buttonColor: Colors.teal),
         // home: AuthPage(),
         routes: {
-          '/': (BuildContext context) => ScopedModelDescendant(
-                builder: (BuildContext context, Widget child, MainModel model) {
-                  return model.user == null ? AuthPage() : ProductsPage(_model);
-                },
-              ),
+          '/': (BuildContext context) =>
+              _model.user == null ? AuthPage() : ProductsPage(_model),
           '/products': (BuildContext context) => ProductsPage(_model),
           '/admin': (BuildContext context) => AdminPage(_model)
         },
