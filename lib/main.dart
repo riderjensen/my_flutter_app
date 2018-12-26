53604f73bea1e9037cb75850b361b5cf78eb41b2
Unknown <riderjensen@gmail.com>
2018-12-26 08:38:11 -0700

    Set API key

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:scoped_model/scoped_model.dart';
+import 'package:map_view/map_view.dart';
 
 import './pages/auth.dart';
 import './pages/products_admin.dart';
@@ -9,6 +10,7 @@ import './scoped-models/main.dart';
 import './models/product.dart';
 
 void main() {
+  MapView.setApiKey("AIzaSyCFdZWbCAE180xvjYhLa4USRC3ydILbyXk");
   runApp(MyApp());
 }
 
